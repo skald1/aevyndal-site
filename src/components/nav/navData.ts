f79384a7d683566c1ba45cb40531a9e963dddbed
SJ Kraus <sjvox@proton.me>
2026-02-19 10:20:20 -0500

    Initial Ævyndal site

---
+++ src/components/nav/navData.ts
@@ -0,0 +1,6 @@
+export const primaryNav = [
+  { href: "/about/", label: "Get Oriented" },
+  { href: "/starting-locations/", label: "Start Now" },
+  { href: "/resources/", label: "Resources" },
+  { href: "/community/", label: "Join In" },
+];
